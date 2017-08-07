62d3ed26997ae87d98aeda5939372be5bf17e2df
TheMaximich <agafonovmak@gmail.com>
2017-08-07 23:36:01 +0300

    Подключен гем ЯндексСловаря.

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -1,3 +1,4 @@
+require 'yandex_dictionary'
 class DictionaryController < ApplicationController
   def index
     @translation ||= Translation.new
@@ -9,11 +10,7 @@ class DictionaryController < ApplicationController
     # @translation.from = translation_hash[:from]
     # @translation.direction = translation_hash[:direction]
 
-    if(@translation.from == '1')
-      @translation.to = 'ПЕРЕВОД'
-    else
-      @translation.to = 'ПЕРЕВОД1243124'
-    end
+    @translation.to = YandexDictionary.translate(@translation.from, 'ru', 'en')
 
     render 'index'
   end
