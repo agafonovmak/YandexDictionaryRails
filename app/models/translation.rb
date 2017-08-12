5e65b21d1570184d80c1dc3180203263298f5029
TheMaximich <agafonovmak@gmail.com>
2017-08-12 19:10:46 +0300

    Убран вывод в консоль параметров.  Изменён путь кнопки "Домой"

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,9 +1,7 @@
 class Translation < ApplicationRecord
   belongs_to :user
   def make_array_from_string
-    puts to
     to.gsub!(/[\[\]]/, '')
-    puts to.split(',')
     return to.split(',')
   end
 end
